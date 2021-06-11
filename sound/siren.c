55881f6c5d394ed6fe615f26b55f223aada9eaf3
Haruka.K <72332745+haruponponpopon@users.noreply.github.com>
2021-06-11 23:26:31 +0900

    Update siren.c

--- sound/siren.c
+++ sound/siren.c
@@ -8,17 +8,21 @@
 #include<math.h>
 //何周期表示したいか
 int main(int argc, char ** argv){
-    int A = atoi(argv[1]);
+    double A = atof(argv[1]);
+    double A1 = A;
     int n = atoi(argv[2]);
-    int sample = 13230;
+    double sample = 13230;
     int num = 0;
     double ra[2] = {880*pow(2,1.0/6.0), 880/pow(2, 1.0/6.0)};
 
     while(num<n*2){
         for (int i=0; i<13230*2; i++){
+            A = A*sin((double)(num*13230*2+i)/(13230*2*2*n)*M_PI);
             short p = A*sin(2* M_PI * ra[num%2] * i/44100);
             write(1,&p,2);
+            A = A1;
         }
+        A = A1;
         num++;
     }
     
